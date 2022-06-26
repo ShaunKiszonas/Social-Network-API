10943689e9737686bf704e5af6fead5150ecd13e
Shaun Kiszonas <smkiszonas@gmail.com>
2022-06-26 15:04:50 -0700

    added models

--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: 'Username is Required',
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'Email is Required',
+      match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+    },
+    thoughts: [],
+    friends: [UserSchema.id],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
